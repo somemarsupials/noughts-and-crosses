01b80ea555bb3215a54fe9e50e610a11286429ad
Theo Breuer-Weil <theobreuerweil@gmail.com>
2020-03-29 11:46:08 +0100

    Add memory of previous board states

--- xo/state/board.py
+++ xo/state/board.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from enum import Enum
 from typing import List, Optional
+import tensorflow as tf
 
 
 class Piece(Enum):
@@ -22,11 +23,12 @@ class Board:
     SIZE = 9
     ROW_LENGTH = 3
 
-    def __init__(self, board_positions):
+    def __init__(self, board_positions, previous=None):
         self._array = board_positions
+        self._previous = previous
 
     @staticmethod
-    def fresh() -> Board:
+    def create() -> Board:
         return Board([Piece.BLANK for _ in range(Board.SIZE)])
 
     def set(self, index_to_set: int, new_value: Piece) -> Board:
@@ -34,11 +36,17 @@ class Board:
             [
                 new_value if index == index_to_set else old_value
                 for index, old_value in enumerate(self._array)
-            ]
+            ],
+            self,
         )
 
     def to_array(self) -> List[Piece]:
         return self._array
+
+    def to_nn_input(self) -> List[int]:
+        noughts = [p == Piece.NOUGHT for p in self._array]
+        crosses = [p == Piece.CROSS for p in self._array]
+        return tf.constant(noughts + crosses, shape=(1, 18))
 
     def has_winner(self) -> bool:
         return bool(self.get_winning_player())
@@ -57,7 +65,6 @@ class Board:
 
     def _run_indices_to_pieces(self, run: List[int]) -> List[Piece]:
         return [self._array[i] for i in run]
-         
 
     def get_winning_player(self) -> Optional[Piece]:
         runs = (
@@ -67,7 +74,7 @@ class Board:
             [0, 4, 8],
             [2, 4, 6],
             [1, 4, 7],
-            [3, 4, 5]
+            [3, 4, 5],
         )
 
         for run in map(self._run_indices_to_pieces, runs):
