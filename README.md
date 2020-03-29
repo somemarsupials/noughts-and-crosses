992d4b56bfa4c8b14a49bb1c5fcdd79d1093a943
Theo Breuer-Weil <theobreuerweil@gmail.com>
2020-03-29 11:46:29 +0100

    Add first version of model builder

---
+++ README.md
@@ -0,0 +1,15 @@
+# Noughts and crosses
+## Usage
+### Setup
+
+You can install the required dependencies using the provided setup script.
+
+`$ sh scripts/setup.sh`
+
+Alternately, run the following to setup and/or activate the project's virtual environment.
+
+`$ source scripts/setup.sh`
+
+### Developing
+
+Use the format and typecheck scripts as you're working!
---
+++ xo/model/model.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from tensorflow import keras, nn
+
+from xo.state import Board
+
+
+class Model:
+    def __init__(self, network):
+        self._model = network
+
+    @staticmethod
+    def create(layer_size=20, num_layers=2, activation=nn.relu) -> Model:
+        inputs = keras.Input(shape=(Board.SIZE * 2,), name="board")
+        layer = inputs
+
+        for index in range(num_layers):
+            layer = keras.layers.Dense(
+                layer_size,
+                activation=activation,
+                name="layer-{}".format(index),
+            )(layer)
+
+        outputs = keras.layers.Dense(1, name="score")(layer)
+        return Model(keras.Model(inputs=inputs, outputs=outputs))
+
+    def predict(self, input_values: List[int]) -> float:
+        return self._model(input_values)
+
+    def train(self, board: Board) -> None:
+        pass
